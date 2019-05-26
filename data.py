bf76733b3a9286a3cd5e0b51f08f7e8d4a8b5aec
Jonathan Campbell <jpcsoup@gmail.com>
2019-05-25 17:58:11 -0700

    data dimensions

--- data.py
+++ data.py
@@ -1,9 +1,26 @@
+# data exploration and testing
+
 import numpy as np
 import pandas as pd
 
-gscores = pd.read_csv('ml-20m/ genome-scores.csv',nrows=1000)
-#gtags
-#links
-#movies
-#ratings
-#tags
+gscores = pd.read_csv('ml-20m/genome-scores.csv')
+gtags = pd.read_csv('ml-20m/genome-tags.csv')
+links = pd.read_csv('ml-20m/links.csv')
+movies = pd.read_csv('ml-20m/movies.csv')
+ratings = pd.read_csv('ml-20m/ratings.csv')
+tags = pd.read_csv('ml-20m/tags.csv')
+
+gscores.shape #(11709768, 3)
+gtags.shape #(1128, 2)
+links.shape #(27278, 3)
+movies.shape #(27278, 3)
+ratings.shape #(20000263, 4)
+tags.shape #(465564, 4)
+
+tags.columns.tolist()
+tags['tag'].nunique() #38643
+tags['movieId'].nunique() #19545
+# average of 1.97 movies per tag
+
+
+
